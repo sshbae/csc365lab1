3c485c37a0e408cf2ace911c6c399068f7b10ff2
Sarah <sarahbae1998@gmail.com>
2020-01-08 10:58:26 -0800

    yey

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,41 +1,56 @@
 import pandas as pd
 
-def studentSearch():
+def teacherSearch(cmd, df):
+    if len(cmd) < 2:
+        return
+    stuDf = df.where(cmd[1] == df["TLastName"]).dropna()
+    for index, row in stuDf.iterrows():
+        print(f'{row["StLastName"]} {row["StFirstName"]}')
+    #print(stuDf)
     
 def handleAsk(cmd, df):
-    if cmd[0][0] = 'S' :
+    if cmd[0][0] == 'S' :
         studentSearch(cmd, df)
-    if cmd[0][0] = 'T' :
+    if cmd[0][0] == 'T' :
         teacherSearch(cmd, df)
 
-    if cmd[0][0] = 'G' :
+    if cmd[0][0] == 'G' :
         gradeSearch(cmd, df)
 
-    if cmd[0][0] = 'B' :
+    if cmd[0][0] == 'B' :
         busSearch(cmd, df)
 
-    if cmd[0][0] = 'A' :
+    if cmd[0][0] == 'A' :
         avgSearch(cmd, df)
 
-    if cmd[0][0] = 'I' :
+    if cmd[0][0] == 'I' :
         inforSearch(cmd, df)
     
 
 def main():
     df = pd.read_csv("./students.txt", header=None, names=["StLastName", "StFirstName", "Grade", "Classroom", "Bus", "GPA", "TLastName", "TFirstName"])
-    cmd = input()
+
+    print(f"""• S[tudent]: <lastname> [B[us]]
+    \n• T[eacher]: <lastname>
+    \n• B[us]: <number>
+    \n• G[rade]: <number> [H[igh]|L[ow]]
+    \n• A[verage]: <number>
+    \n• I[nfo]
+    \n• Q[uit]""")
+    cmd = input(">>")
+
     while cmd is not "Q":
-        print(f"• S[tudent]: <lastname> [B[us]]
+        cmd = cmd.split()
+        handleAsk(cmd, df)
+        print(f"""• S[tudent]: <lastname> [B[us]]
         \n• T[eacher]: <lastname>
         \n• B[us]: <number>
         \n• G[rade]: <number> [H[igh]|L[ow]]
         \n• A[verage]: <number>
         \n• I[nfo]
-        \n• Q[uit]")
-        cmd = cmd.split()
-        handleAsk(cmd, df)
+        \n• Q[uit]""")
         cmd = input()
-   return 
+    return 
 
 if __name__ == '__main__':
     main()
