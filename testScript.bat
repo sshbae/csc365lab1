d442676a14284879a4303b878481e05178222753
Sarah <sarahbae1998@gmail.com>
2020-01-13 11:04:36 -0800

    testScript works for unix/linux

--- testScript.bat
+++ testScript.bat
@@ -1,4 +1,3 @@
-title: "csc365 lab1"
-@echo off
-    "C:\Users\Nicole Schwartz\Anaconda3\python.exe" "C:\Users\Nicole Schwartz\Anaconda3\csc365\csc365lab1\schoolsearch.py" 
+exec > "./output.txt" 2>&1
+    "python3" "./schoolsearch.py" 
   
